a1880f2abf2355441b6601a5664014b405943126
Nicholas Kniola <nickkniola@me.com>
2021-01-22 05:04:10 +0000

    Revise function for more nextButton clicks

--- javascript-automation/main.js
+++ javascript-automation/main.js
@@ -1,5 +1,5 @@
 const jobs = document.querySelectorAll('li.jobs-search-results__list-item a.job-card-list__title');
-let counter = 3000
+let counter = 2000
 
 jobs.forEach(job => {
   setTimeout(() => {
@@ -9,19 +9,25 @@ jobs.forEach(job => {
       if (applyButton) {
         applyButton.click();
       }
-    }, 1000);
+    }, 0);
     setTimeout(() => {
       const nextButton = document.querySelector('button.artdeco-button--primary');
       if (nextButton) {
         nextButton.click();
       }
-    }, 1000);
+    }, 500);
      setTimeout(() => {
       const nextButton = document.querySelector('button.artdeco-button--primary');
       if (nextButton) {
         nextButton.click();
       }
-    }, 1000);
+     }, 1000);
+    setTimeout(() => {
+      const nextButton = document.querySelector('button.artdeco-button--primary');
+      if (nextButton) {
+        nextButton.click();
+      }
+    }, 1500);
   }, counter);
-  counter += 3200;
+  counter += 2000;
 });
